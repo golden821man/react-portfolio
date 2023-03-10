7631695c9cbcbd0b526ede2b80722baaa91aafc4
golden821man <goldenman1991821@gmail.com>
2023-03-11 03:08:34 +0900

    Create portfolio

---
+++ src/content_option.js
@@ -0,0 +1,208 @@
+const logotext = "STEVEN GOLDEN";
+const meta = {
+    title: "Steven GOlden",
+    description: "I’m Steven Golden Full stack developer _ Blockchain developer",
+};
+
+const introdata = {
+    title: "I’m Steven Golden",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        // third: "I am looking for challenging projects",
+    },
+    description: "Full Stack & Blockchain developer",
+    your_img_url: "assets/photo-1514790193030-c89d266d5a9d.jfif",
+};
+
+const dataabout = {
+    title: "About myself",
+    aboutme: "As a Full Stack Developer with 6+ years of experience, my primary expertise lies in building front and I have extensive experience working with React, and recently jumped in GO. I always try to write clean and well-documented codes. Also I am highly skilled in a variety of platforms, development frameworks and programming languages, and especially algorithms and data structures. When I’m not working I enjoy solving DSA problems and climbing. Coding is not my job, it’s a part of my life. I enjoy coding like games",
+};
+const worktimeline = [{
+        jobtitle: "Senior Full Stack Developer",
+        where: "Flare",
+        date: "2021",
+    },
+    {
+        jobtitle: "Full Stack Developer",
+        where: "Upwork,Freelancer",
+        date: "2020",
+    },
+    {
+        jobtitle: "Blockchain Developer",
+        where: "ThimbleGang Puerto Real",
+        date: "2019",
+    },
+    {
+        jobtitle: "Senior Full Stack Developer",
+        where: "Ratio Finance Houston",
+        date: "2017",
+    },
+];
+
+const skills = [
+    {
+        name: "CSS",
+        value: 95,
+    },
+    {
+        name: "Bootstrap",
+        value: 95,
+    },
+    {
+        name: "Tailwind",
+        value: 95,
+    },
+    {
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Djano",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 95,
+    },
+    {
+        name: "React",
+        value: 95,
+    },
+    {
+        name: "React Native",
+        value: 90,
+    },
+    {
+        name: "Vue",
+        value: 85,
+    },
+    {
+        name: "Next",
+        value: 90,
+    },
+    {
+        name: "Nuxt",
+        value: 85,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+    {
+        name: "Rust",
+        value: 85,
+    },
+    {
+        name: "Solana",
+        value: 80,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "",
+    },
+    {
+        title: "Mobile Apps",
+        description: "",
+    },
+    {
+        title: "Figma Design",
+        description: "",
+    },
+];
+
+const dataportfolio = [{
+        img: "assets/algonrich.png",
+        desctiption: "Algonrich",
+        link: "https://www.algonrich.com",
+    },
+    {
+        img: "assets/nimo.png",
+        desctiption: "nimo",
+        link: "https://nimoindustries.com/",
+    },
+    {
+        img: "assets/swye360.png",
+        desctiption: "SWYE360",
+        link: "https://www.swye360.com/",
+    },
+    {
+        img: "assets/pieceofcake.png",
+        desctiption: "Piece of Cake",
+        link: "https://www.pieceofcakeinc.com/",
+    },
+    {
+        img: "assets/crosswise.png",
+        desctiption: "CROSSWISE",
+        link: "https://crosswise.finance/",
+    },
+    {
+        img: "assets/dissrup.png",
+        desctiption: "DISSRUP",
+        link: "https://dissrup.com",
+    },
+
+    {
+        img: "assets/fazwaz.png",
+        desctiption: "FazWaz",
+        link: "https://www.fazwaz.com",
+    },
+    {
+        img: "assets/yogaia.png",
+        desctiption: "yogaia",
+        link: "https://yogaia.com",
+    },
+    {
+        img: "assets/montex.png",
+        desctiption: "Montex",
+        link: "https://montex.market/",
+    },
+    {
+        img: "assets/sporthitters.png",
+        desctiption: "SPORTHITEERS",
+        link: "https://sporthitters.com/",
+    },
+    // {
+    //     img: "https://picsum.photos/400/?grayscale",
+    //     desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/700/?grayscale",
+    //     desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "goldenman0821@gmail.com",
+    // YOUR_FONE: "(555)123-4567",
+    // description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/golden821man",
+    // facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    // twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
